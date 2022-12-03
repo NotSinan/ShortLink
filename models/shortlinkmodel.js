9b930c6a926bc9cc64c2f736a1febd301dcf51f0
Sinan <haxsmith77@gmail.com>
2022-12-04 03:30:55 +1100

    Added shortlink model

---
+++ models/shortlinkmodel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const shortlinkSchema = new mongoose.Schema({
+    full: {
+        type: String,
+        required: true
+    },
+
+    short: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Shortlink', shortlinkSchema)

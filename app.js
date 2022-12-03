87c2a173ae1c00197b2fffa5b7da700db7016d9f
Sinan <haxsmith77@gmail.com>
2022-12-04 03:31:16 +1100

    Updated index and app

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var shortlinksRouter = require('./routes/shortlinks');
 
 var app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/shortlink', () => {
+  console.log('connected')
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/shortlinks', shortlinksRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

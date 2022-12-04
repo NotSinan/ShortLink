1702d5df60b29c847c435ddfd01a0822f9b17939
Sinan <haxsmith77@gmail.com>
2022-12-04 22:15:07 +1100

    Updated index to have crud opertations of posting urls and getting urls

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const Shortlink = require('../models/shortlinkmodel');
 
+router.get('/', (req, res, next) => {
+    res.render("index")
+});
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render("index")
+router.post('/add', async (req, res, next) => {
+  Shortlink.findOne({short: req.body.short}, async (err, existingShortlink) => {
+    if (existingShortlink == null) {
+      await Shortlink.create({full: req.body.full, short: req.body.short})
+      res.send("Added.")
+    } else {
+      res.sendStatus(400);
+    }
+  });
+});
+
+router.get('/:shortlink', async (req, res) => {
+  const shortlink = await Shortlink.findOne({short: req.params.shortlink}) 
+  if (shortlink == null) {
+      res.sendStatus(404)
+  } else {
+    res.redirect(shortlink.full);
+  }
 });
 
 module.exports = router;

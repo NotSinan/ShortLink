aa0348a7263c6806adcd2f9ed77bb60fe8fd5490
Sinan <haxsmith77@gmail.com>
2022-12-17 23:24:44 +1100

    Added connection.ts

---
+++ src/connection.ts
@@ -0,0 +1,11 @@
+import { AppDataSource } from "./data-source";
+
+export const initialiseDataSource = () => {
+  AppDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err);
+    });
+};

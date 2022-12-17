59c546da4b400614a54b5ca81d76dc431d287af3
Sinan <haxsmith77@gmail.com>
2022-12-17 20:20:55 +1100

    Added shortlink entity with typeorm

---
+++ src/entities/Shortlink.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, BaseEntity, ObjectIdColumn } from "typeorm";
+
+@Entity()
+export class Shortlink extends BaseEntity {
+  @ObjectIdColumn()
+  _id!: Object;
+
+  @Column()
+  short_link!: string;
+
+  @Column()
+  full_link!: string;
+
+  @Column()
+  clicks!: number;
+}
